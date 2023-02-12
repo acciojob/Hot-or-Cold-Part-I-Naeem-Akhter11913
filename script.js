b46e093ad4c99a00da2b40dd7d2a8bfff97fdd19
Naeem-Akhter11913 <122079648+Naeem-Akhter11913@users.noreply.github.com>
2023-02-12 18:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+function guess(){
+	var userNumber = document.getElementById('guess').value;
+	var rand = Math.random();
 
+	if(userNumber == rand){
+		document.getElementById('respond').innerHTML = "Hot";
+	}else{
+		document.getElementById('respond').innerHTML = "Cold";
+	}
+}
